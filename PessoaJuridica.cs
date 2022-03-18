b2cc58d84cf54821a889ca4f5d1189658d67292b
PgLESv <81176654+PgLESv@users.noreply.github.com>
2022-03-18 12:49:11 -0300

    Initial commit

---
+++ PessoaJuridica.cs
@@ -0,0 +1,12 @@
+namespace ENCONTROREMOTO3
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string? cnpj { get; set; }
+
+        public string? razaoSocial { get; set; }
+
+        public override void PagarImposto(float salario){
+        }
+    }
+}
